e7c25313fe655b1e82dc3d4172a882f22fc8b778
JeffBuss <turbocapslock@gmail.com>
2016-12-28 19:41:05 -0700

    Write random number generator that sets randomNumber state on page load

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -10,14 +10,26 @@ export default class App extends Component {
       min: 0,
       max: 100,
       lastGuess: '',
+      randomNumber: null,
     };
   }
+
+  componentDidMount() {
+    this.newRandomNumber();
+  }
+
+  newRandomNumber() {
+    this.setState({
+      randomNumber: Math.floor(Math.random() *
+      (this.state.max - this.state.min + 1)) + this.state.min });
+  }
+
   render() {
     return (
       <div className = 'main-app'>
         <header className='title'>Number Guesser Main App</header>
         <UserInput
-          
+
         />
         <Response />
       </div>
--- lib/components/Response.jsx
+++ lib/components/Response.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Response = () => {
   return (
-    <div className='response-field'>Response Field</div>
+    <div className='response-field'></div>
   );
 };
 
