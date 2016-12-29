a0e32709e35758ef3643143b2b381947411cc83f
JeffBuss <turbocapslock@gmail.com>
2016-12-28 19:29:58 -0700

    Link up buttons and input fields to functions

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -4,11 +4,21 @@ import UserInput from './UserInput.jsx'
 import Response from './Response.jsx'
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      min: 0,
+      max: 100,
+      lastGuess: '',
+    };
+  }
   render() {
     return (
       <div className = 'main-app'>
         <header className='title'>Number Guesser Main App</header>
-        <UserInput />
+        <UserInput
+          
+        />
         <Response />
       </div>
     );
--- lib/components/UserInput.jsx
+++ lib/components/UserInput.jsx
@@ -2,10 +2,64 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 export default class UserInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userGuess: '',
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({ userGuess: '' });
+    console.log('Submit');
+  }
+
+  handleClear(e) {
+    e.preventDefault();
+    this.setState({ userGuess: '' });
+    console.log('Clear');
+  }
+
+  handleReset(e) {
+    e.preventDefault();
+    console.log('Reset');
+  }
+
   render() {
-    console.log('input');
     return (
-      <div className='user-input'>Buttons and stuff</div>
+      <div className='user-input'>
+
+        <input
+          className='guess-input'
+          type='number'
+          placeholder='Make a guess'
+          value={this.state.userGuess}
+          onChange={(e) => this.setState({ userGuess: e.target.value })}
+        />
+
+        <button
+          className='guess-button'
+          onClick={this.handleSubmit.bind(this)}
+        >
+        Guess
+        </button>
+
+        <button
+          className='clear-button'
+          onClick={this.handleClear.bind(this)}
+        >
+        Clear
+        </button>
+
+        <button
+          className='reset-button'
+          onClick={this.handleReset.bind(this)}
+        >
+        Reset Game
+        </button>
+
+      </div>
     );
   }
 }
